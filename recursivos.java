1a0bf0244a3aac954d370564d9d7e7869ad77708
Vinícius Dias <108445811+vinipdiass@users.noreply.github.com>
2024-08-12 20:25:54 -0300

    exercicio 3 pronto

--- recursivos.java
+++ recursivos.java
@@ -1,6 +1,7 @@
 public static void main(String [] args){
     System.out.println("Fatorial de 10: " + fatorial(10));
     System.out.println("Somatorio de 10: " + somatorio(-10));
+    System.out.println("Sétimo elemento da sequencia de fibonacci: " + fibonacci(7));
 }
 
 
@@ -34,4 +35,22 @@ static public int fatorial(int n){
     return n + somatorio (n-1);
  }
 
+/*
+ FIBONACCI
+Este método é válido para o seguinte formato da lista: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987...
+Caso base | N < 2 -> N
+Caso de erro | N < 0
+Caso recursivo 
+
+ */
+
+ static public int fibonacci(int n){
+    if (n < 2){
+        return n;
+    }else if (n < 0){
+        return -1;
+    }
+    return fibonacci(n-1) + fibonacci (n-2);
+    
+ }
  
