e77537b29840cb8c094e9d123bd00610ee878c9f
Vinícius Dias <108445811+vinipdiass@users.noreply.github.com>
2024-08-12 20:03:44 -0300

    melhorias

--- recursivos.java
+++ recursivos.java
@@ -1,9 +1,10 @@
 public static void main(String [] args){
-    System.out.println(fatorial(10));
+    System.out.println("Fatorial de 10: " + fatorial(10));
 }
 
 
 /*
+FATORIAL
 Caso base |  N=1 -> 1
 Caso de erro | N <= 0 
 Caso recursivo | N > 1 -> N * fatorial(n-1)
@@ -18,3 +19,8 @@ static public int fatorial(int n){
 
     return n * fatorial(n-1);
 }
+
+/*
+ SOMATÓRIO
+ Caso base | N = 
+ */
